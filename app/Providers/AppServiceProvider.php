6bd2e903ad0cd81b9828bcba0348ca002f22a5c9
Vaseem <vaseem9389@gmail.com>
2026-08-21 19:33:09 +0530

    Fix HTTPS asset loading

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,21 @@
 <?php
 
 namespace App\Providers;
-use Illuminate\Support\Facades\Schema;
+
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        Schema::defaultStringLength(191);
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
